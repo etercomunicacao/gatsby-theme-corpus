570a6c82fdcdcd8e367d8656a56f02e81264b8c6
Eduardo Oliveira <eduardo@eter.ppg.br>
2020-07-07 15:16:53 -0300

    Blog front cards dobrando

--- src/pages/sections/blog-front.tsx
+++ src/pages/sections/blog-front.tsx
@@ -31,9 +31,7 @@ export default function BlogFrontPage(props) {
             id
             slug
             titulo
-            excerpt {
-              json
-            }
+            dataHora(formatString: "MMMM, YYYY", locale: "pt-br")
             imagemDestaque {
               arquivo {
                 fluid {
@@ -54,37 +52,48 @@ export default function BlogFrontPage(props) {
       <div id="spacer" className="bg-corpus-baby-blue-200 order-1 lg:absolute z-0 inset-x-0 top-0 w-screen h-24 lg:h-64" />
       <div className="container mx-auto">
         <h1 className="font-display text-4xl text-corpus-beige-300 font-thin ml-6 w-full">Blog da Corpus</h1>
-        <div className="flex flex-row mt-10 w-full">
-        {Posts.edges.map(({ node }, i) => {
-        const {
-        id,
-        slug,
-        titulo,
-        excerpt,
-        imagemDestaque
-        } = node
-        const { fluid } = imagemDestaque.arquivo
-        
-        return (
-          <Link to={`/blog/${slug}`} className="w-1/3 mx-6 h-auto ">
-            <div className={`bg-white class_${id} w-full h-full relative`} key={i} id={slug}>
-              <div className="w-full h-64 object-cover">
-                <Img fluid={fluid} className="w-full h-full" />
-              </div>
-              <div className="w-full h-full px-5 py-3">
-                <ul className="inline-flex">
-                  {Posts.group.map(categoria => {
-                    return <li className="block text-black"><Link to={`/categorias/${kebabCase(categoria.fieldValue)}/`}>{categoria.nome}</Link></li>
-                  })}
-                </ul>
-                <h2 className="font-display text-corpus-blue-500 text-xl">{titulo}</h2>
-                <RichText input={excerpt} />
-                <span className="ml-auto mt-auto uppercase font-display font-semibold text-corpus-grayish-blue-400 w-1/3 ">Leia Mais</span>
-              </div>
-            </div>
-          </Link>
-        )
-        })}
+        <div className="mt-10 w-full">
+          <ol className="articles grid grid-cols-3 gap-20 justify-center grid-rows-1">
+            {Posts.edges.map(({ node }, i) => {
+
+              const {
+                id,
+                slug,
+                titulo,
+                dataHora,
+                imagemDestaque
+              } = node
+              
+              const { fluid } = imagemDestaque.arquivo
+
+              return (
+                <li className={`articles__article id__${id} cursor-pointer block relative h-auto`} key={i} id={slug} >
+                  <Link className="articles__link" to={`/blog/${slug}/`}>
+                    <Img fluid={fluid} className="w-full h-full" />
+                    <div className="articles__content articles__content--lhs bg-white">
+                      <h2 className="articles__title">{titulo}</h2>
+                      <div className="articles__footer">
+                        {Posts.group.map(categoria => {
+                          return <p><Link to={`/categorias/${kebabCase(categoria.fieldValue)}/`}>{categoria.nome}</Link></p>
+                        })}
+                        <span>{dataHora}</span>
+                      </div>
+                    </div>
+                    <div className="articles__content articles__content--rhs">
+                      <h2 className="articles__title">{titulo}</h2>
+                      <div className="articles__footer">
+                        {Posts.group.map(categoria => {
+                          return <p><Link to={`/categorias/${kebabCase(categoria.fieldValue)}/`}>{categoria.nome}</Link></p>
+                        })}
+                        <span>{dataHora}</span>
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              )
+            })}
+          
+          </ol>
         
         </div>
       </div>
